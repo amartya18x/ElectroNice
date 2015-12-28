5d02922fbefb557babc17655dc570c736e044ce9
Amartya <amartya@cse.iitk.ac.in>
2015-12-29 04:15:01 +0530

    Added cost function

--- model.py
+++ model.py
@@ -3,10 +3,10 @@ import theano
 import theano.tensor as T
 import numpy as np
 from theano.tensor.shared_randomstreams import RandomStreams
+import theano.tensor.nlinalg
 
 class NiceACDC(object):
-    def __init__(self,rng,input,n_in):
-        self.rng = rng
+    def __init__(self,input,n_in):
         self.inp = input
         self.n_in = n_in
         ##Build the shared parameters
@@ -20,17 +20,107 @@ class NiceACDC(object):
         [A4_values,D4_values,Kx4,Ky4] = coup4.params
         coup5 = Params1(self.n_in)
         [A5_values,D5_values,Kx5,Ky5] = coup5.params
+        self.params = coup1.params + coup2.params + coup3.params + coup4.params + coup5.params
         ##Build the layers
         self.layer1 = electronice(
-            srng,inp,n_in,A=A_values,D=D_values,Kx=Kx,Ky=Ky)
-        layer2 = electronice(rng=srng,n_in=n_in,inp=layer1.output,A=A_values,D=D_values,Kx=Kx,Ky=Ky ,inverse=True)
-        cost = T.sum(layer2.output)
-        params = layer1.params + layer2.params 
-        grad = T.grad(cost,params)
-        fn = theano.function([inp],[layer1.output])
-        gn = theano.function([inp],grad)
-        input  =np.random.uniform(size=(n_in,))
-        print input
-        print gn(input)
-        print fn(input)
-
+            inp = input,
+            n_in = n_in,
+            A = A1_values,
+            D = D1_values,
+            Kx = Kx1,
+            Ky = Ky1
+        )
+        self.layer2 = electronice(
+            n_in = n_in,
+            inp = self.layer1.output,
+            A = A2_values,
+            D = D2_values,
+            Kx = Kx2,
+            Ky = Ky2
+        )
+        self.layer3 = electronice(
+            n_in = n_in,
+            inp = self.layer2.output,
+            A = A3_values,
+            D = D3_values,
+            Kx = Kx3,
+            Ky = Ky3
+        )
+        self.layer4 = electronice(
+            n_in = n_in,
+            inp = self.layer3.output,
+            A = A4_values,
+            D = D4_values,
+            Kx = Kx4,
+            Ky = Ky4
+        )
+        self.layer5 = electronice(
+            n_in = n_in,
+            inp = self.layer4.output,
+            A = A5_values,
+            D = D5_values,
+            Kx = Kx5,
+            Ky = Ky5
+        )
+        wrt = self.inp
+        expr = self.layer5.output
+        self.Jacob , updates = theano.scan(lambda i, y,x : theano.gradient.jacobian(expr[i], x), sequences=T.arange(expr.shape[0]), non_sequences=[expr,wrt]) 
+        self.logDet  = theano.tensor.log(theano.tensor.nlinalg.Det()(self.Jacob))
+        self.layer6 = electronice(
+            n_in = n_in,
+            inp = self.layer5.output,
+            A = A5_values,
+            D = D5_values,
+            Kx = Kx5,
+            Ky = Ky5,
+            inverse = True 
+        )
+        self.layer7 = electronice(
+            n_in = n_in,
+            inp = self.layer6.output,
+            A = A4_values,
+            D = D4_values,
+            Kx = Kx4,
+            Ky = Ky4,
+            inverse = True
+        )
+        self.layer8 = electronice(
+            n_in = n_in,
+            inp = self.layer7.output,
+            A = A3_values,
+            D = D3_values,
+            Kx = Kx3,
+            Ky = Ky3,
+            inverse = True
+        )
+        self.layer9 = electronice(
+            n_in = n_in,
+            inp = self.layer8.output,
+            A = A2_values,
+            D = D2_values,
+            Kx = Kx2,
+            Ky = Ky2,
+            inverse = True
+        )
+        self.layer10 =  electronice(
+            n_in = n_in,
+            inp = self.layer9.output,
+            A = A1_values,
+            D = D1_values,
+            Kx = Kx1,
+            Ky = Ky1,
+            inverse = True
+        )
+        self.output = self.layer10.output
+        self.prior =T.sum( -T.log(1+T.exp(self.layer5.output)) - T.log(1 + T.exp(-1*self.layer5.output)))
+        self.cost = self.prior + self.logDet
+        self.grad = T.grad(self.cost,self.params)
+inp = T.vector("inp")
+n_in = 5
+ANice = NiceACDC(input = inp, n_in = n_in)
+fn = theano.function([inp],[ANice.output,ANice.prior,ANice.logDet])
+gn = theano.function([inp],ANice.grad)
+input  =np.random.uniform(size=(n_in,))
+print input
+#print gn(input)
+print fn(input)
--- nicelectro.py
+++ nicelectro.py
@@ -17,10 +17,10 @@ class electronice(object):
         self.A = A
         self.D = D
         if inverse == False:
-            h1 = T.dot(inp, T.diag(self.A)) 
-            h2 = T.dot(h1,C)
-            h3 = T.dot(h2,T.diag(self.D))
-            self.lin_output = T.dot(h3,C_t)
+            self.h1 = T.dot(inp, T.diag(self.A)) 
+            self.h2 = T.dot(self.h1,C)
+            self.h3 = T.dot(self.h2,T.diag(self.D))
+            self.lin_output = T.dot(self.h3,C_t)
             self.output,_ = theano.scan(lambda i,list_x : self.knot_activ(list_x[i]), sequences=T.arange(self.lin_output.shape[0]), non_sequences=self.lin_output)
         else:
             self.h1,_ = theano.scan(lambda i,list_x : self.knot_activ_inverse(list_x[i]), sequences=T.arange(inp.shape[0]), non_sequences=inp)
@@ -50,13 +50,13 @@ class electronice(object):
 class Params1(object):
     def __init__(self,n_in,A=None,D=None,Kx=None,Ky=None):
         if A is None:
-            A_values = np.random.normal(0,0.02,(n_in,))
+            A_values = np.random.normal(0,0.2,(n_in,))
         else:
             A_values = A
         A = theano.shared(A_values,borrow=True)
         
         if D is None:
-            D_values = np.random.normal(0,0.02,(n_in,))
+            D_values = np.random.normal(0,0.2,(n_in,))
         else:
             D_values = D
         D = theano.shared(D_values, borrow=True)
@@ -73,22 +73,22 @@ class Params1(object):
 
 
 
-        
-inp = T.vector('inp')
-srng = RandomStreams(seed=12345)
-n_in = 5
-#A_values = np.random.normal(0,0.02,(n_in,))
-#D_values = np.random.normal(0,0.02,(n_in,))
-coup1 = Params1(n_in)
-[A_values,D_values,Kx,Ky] = coup1.params
-layer1 = electronice(inp,n_in,A=A_values,D=D_values,Kx=Kx,Ky=Ky)
-layer2 = electronice(n_in=n_in,inp=layer1.output,A=A_values,D=D_values,Kx=Kx,Ky=Ky ,inverse=True)
-cost = T.sum(layer2.output)
-params = layer1.params + layer2.params 
-grad = T.grad(cost,params)
-fn = theano.function([inp],[layer1.output])
-gn = theano.function([inp],grad)
-input  =np.random.uniform(size=(n_in,))
-print input
-print gn(input)
-print fn(input)
+if __name__ == '__main__':        
+    inp = T.vector('inp')
+    srng = RandomStreams(seed=12345)
+    n_in = 5
+    #A_values = np.random.normal(0,0.02,(n_in,))
+    #D_values = np.random.normal(0,0.02,(n_in,))
+    coup1 = Params1(n_in)
+    [A_values,D_values,Kx,Ky] = coup1.params
+    layer1 = electronice(inp,n_in,A=A_values,D=D_values,Kx=Kx,Ky=Ky)
+    layer2 = electronice(n_in=n_in,inp=layer1.output,A=A_values,D=D_values,Kx=Kx,Ky=Ky ,inverse=True)
+    cost = T.sum(layer2.output)
+    params = layer1.params + layer2.params 
+    grad = T.grad(cost,params)
+    fn = theano.function([inp],[layer2.output])
+    gn = theano.function([inp],grad)
+    input  =np.random.uniform(size=(n_in,))
+    print input
+    #print gn(input)
+    print fn(input)
